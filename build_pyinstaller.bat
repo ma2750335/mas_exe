82efc9e56c54bc8b92cafac0c6a119037abf3b68
SamLin <ma2750335@gmail.com>
2026-05-26 16:27:54 +0800

    Remove unnecessary --add-data option from PyInstaller build script

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -29,7 +29,6 @@ py -m PyInstaller ^
   -i "%ICON_FILE%" ^
   --add-binary "%LOGO_PNG%;." ^
   --add-binary "%LOGO_ICO%;." ^
-  --add-data ".env;." ^
   --hidden-import MetaTrader5 ^
   --exclude-module scipy.special._cdflib ^
   --exclude-module pysqlite2 ^
